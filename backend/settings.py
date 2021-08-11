534d64edfc4b88b9629aa44fd33a54b4f583a671
touhidalamin123@gmail.com <touhidalamin123@gmail.com>
2021-08-11 14:18:34 +0600

    Change Settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l$8i=*16-wmcdnuh#@1mnx9$4-ka%%iem(=yf$@(eqdtr#8#)z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['techtouhid.herokuapp.com', '127.0.0.1:8000', 'localhost']
@@ -70,12 +70,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
